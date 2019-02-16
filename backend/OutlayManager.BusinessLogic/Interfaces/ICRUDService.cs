fa3ba1b0e7e953c1a60cd22bf6bd07d4a86e7365
Nacht-Reiter <maxkolpak@ukr.net>
2019-02-16 19:49:58 +0200

    Crud service

---
+++ backend/OutlayManager.BusinessLogic/Interfaces/ICRUDService.cs
@@ -0,0 +1,17 @@
+﻿using OutlayManager.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutlayManager.BusinessLogic.Interfaces
+{
+    public interface ICRUDService<TModel, TDTO> where TModel : Model where TDTO : class
+    {
+        Task<TDTO> GetAsync(int id);
+        Task<IEnumerable<TDTO>> GetAllAsync();
+        Task<bool> AddAsync(TDTO item);
+        Task<bool> DeleteAsync(int id);
+        Task<TDTO> Update(TDTO item);
+    }
+}
---
+++ backend/OutlayManager.BusinessLogic/Services/CRUDService.cs
@@ -0,0 +1,79 @@
+﻿using AutoMapper;
+using OutlayManager.BusinessLogic.Interfaces;
+using OutlayManager.DataAccess.Models;
+using OutlayManager.DataAccess.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutlayManager.BusinessLogic.Services
+{
+    public class CRUDService<TModel, TDTO> : ICRUDService<TModel, TDTO> where TModel : Model where TDTO : class
+    {
+        protected readonly IUnitOfWork uow;
+        protected readonly IMapper mapper;
+
+        public CRUDService(IUnitOfWork uow, IMapper mapper)
+        {
+            this.mapper = mapper;
+            this.uow = uow;
+        }
+
+
+        public virtual async Task<bool> AddAsync(TDTO item)
+        {
+            if (uow != null)
+            {
+                await uow.Repository<TModel>().CreateAsync(mapper.Map<TModel>(item));
+                return true;
+            }
+            return false;
+        }
+
+        public virtual async Task<bool> DeleteAsync(int id)
+        {
+            if (uow != null)
+            {
+                await uow.Repository<TModel>().DeleteAsync(id);
+                return true;
+            }
+            return false;
+        }
+
+        public virtual async Task<IEnumerable<TDTO>> GetAllAsync()
+        {
+            if (uow != null)
+            {
+                var item = await uow.Repository<TModel>().GetAllAsync();
+                if(item != null)
+                {
+                    return mapper.Map<IEnumerable<TDTO>>(item);
+                }
+            }
+            return null;
+
+                
+        }
+
+        public virtual async Task<TDTO> GetAsync(int id)
+        {
+            if (uow != null)
+            {
+                var items = await uow.Repository<TModel>().GetAsync(id);
+                return mapper.Map<TDTO>(items);
+            }
+            return null;
+        }
+
+        public virtual async Task<TDTO> Update(TDTO item)
+        {
+            if (uow != null)
+            {
+                var items = uow.Repository<TModel>().Update(mapper.Map<TModel>(item));
+                return mapper.Map<TDTO>(items);
+            }
+            return null;
+        }
+    }
+}
